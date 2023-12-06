7dcc81b06756a5c7cbe2dcd86ef8bf53e0f4ae32
xdjohn99 <102330218+xdjohn99@users.noreply.github.com>
2023-12-07 00:19:51 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -210,7 +210,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }{ value: "http://128.0.0.1:25500/sub?" }],
         remoteConfig: [
           {
             label: "universal",
